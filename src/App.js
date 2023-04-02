81010991c47e7c8db805a071125a9553a7606668
Humeyra02 <humeyrasimsek582@gmail.com>
2023-04-02 15:38:58 +0100

    redux structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 
+import { useSelector } from "react-redux";
+
 function App() {
+  const allStates=useSelector(state=>state)
+  console.log(allStates)
   return (
     <div>
       <h1>Library</h1>
@@ -9,3 +13,52 @@ function App() {
 }
 
 export default App;
+
+
+/**
+ * function App() {
+  const allStates=useSelector(state=>state)
+  console.log(allStates.booksState)  <= bu sadece ozel bir state istedigimizde kullanacagumiz yontem
+                                        yani butun stateler icinde bana sadece booksState getir
+  return (
+    <div>
+      <h1>Library</h1>
+    </div>
+  );
+}
+
+ */
+
+/**
+ * SADECE BOOKSSTATE yada TEK BIR STATE E ABONE OLMAK ISTERSEK BUNU YAPABILIRIZ
+ *
+ * 
+ * function App() {
+  const booksState=useSelector(state=>state.bookState)
+                                    (dondurulen tum stateler icindeki bookstate i dondur demek)
+  console.log(booksState)
+  return (
+    <div>
+      <h1>Library</h1>
+    </div>
+  );
+}
+ */
+
+/**
+ * function App() {
+  const {booksState,categoriesState}=useSelector(state=>state)
+          // console.log(booksState)
+          // console.log(categoriesState)
+
+          >>>> statelere ayri ayri abone olduk. 
+
+          >>>> es6 ile gelen destructing method yani tersten islem.
+
+  return (
+    <div>
+      <h1>Library</h1>
+    </div>
+  );
+}
+ */
---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,16 @@
+const actionTypes ={
+    //butun typelari burda tutucaz. 
+    bookActions:{
+        /*bu bir degisken*/ 
+        GET_BOOKS_START:"GET_BOOKS_START",
+        GET_BOOKS_SUCCESS:"GET_BOOKS_SUCCESS",
+        GET_BOOKS_FAIL:"GET_BOOKS_FAIL"
+    },
+    categoryActions:{
+        GET_CATEGORIES_START:"GET_CATEGORIES_START",
+        GET_CATEGORIES_SUCCESS:"GET_CATEGORIES_SUCCESS",
+        GET_CATEGORIES_FAIL:"GET_CATEGORIES_FAIL"
+    }
+}
+
+export default actionTypes
